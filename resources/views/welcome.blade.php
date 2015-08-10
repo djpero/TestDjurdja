df24cf9a686294c20c490d69298a7b2ec04f728f
Pero Knezevic <djpero.84@gmail.com>
2015-08-10 13:31:47 +0200

    Adding styles, create method rows

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,42 +3,28 @@
     <head>
         <title>Laravel</title>
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link href="//fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
     </head>
     <body>
+        <header>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </header>
         <div class="container">
+            <h1>Delivery options</h1>
             <div class="content">
-                <div class="title">Laravel 52</div>
+                <div class="method">
+                    <div class="title">
+                        <p>Delivery Method 0</p>
+                    </div>
+                    <div class="value">
+                        <input id="dm0" type="number" step="any"/>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
